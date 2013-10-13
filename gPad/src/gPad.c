41e28d81e51e7611f69b67c46097f5daa0b137a7
janague <janague@gmail.com>
2013-10-13 22:49:31 +0200

    fix #3: Read text from a file

--- gPad/src/gPad.c
+++ gPad/src/gPad.c
@@ -26,9 +26,11 @@
  * $gPad$
  */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <gtk/gtk.h>
+
+#define BUFFER_SIZE 1024
 
 static gboolean
 on_delete_event (   GtkWidget *widget,
@@ -40,29 +42,8 @@ on_delete_event (   GtkWidget *widget,
     return GDK_EVENT_PROPAGATE;
 }
 
-static void read_file(char *filename,
-                    GtkTextBuffer *buffer) 
-{   
-    g_printf ("File %s.\n",filename);
-
-    gtk_text_buffer_set_text (buffer, "Content of the file------", -1);
-
-    FILE *inFile;
-    char c;
-    inFile=fopen(filename,"r");
-
-    if(inFile==NULL)
-    {
-        printf("An error occoured!");
-        exit(1);
-    }
-
-//    while((c=fgetc(inFile))!=EOF)
-//        gtk_text_buffer_set_text (buffer, &c, 1);
-
-    fclose(inFile);
-}
-
+void read_file ( char *filename,
+                 GtkTextBuffer *buffer); 
 
 int 
 main(int argc, char **argv)
@@ -76,7 +57,7 @@ main(int argc, char **argv)
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL); 
     gtk_window_set_title(   GTK_WINDOW(window),
-                            "gImageViewer");
+                            "gPad");
 
     g_signal_connect (  window,
                         "delete-event",
@@ -96,4 +77,38 @@ main(int argc, char **argv)
     gtk_main();
 
     return 0;
+}
+
+
+//_____________________________________________________________________________
+// read_file - Read text file and write in GtkTextBuffer
+//      filename: path + file name
+//      buffer: pointer to a buffer
+void read_file ( char *filename,
+                 GtkTextBuffer *buffer)
+{
+    char buf[BUFFER_SIZE];
+    FILE *inFile;
+
+    GtkTextMark *mark;
+    GtkTextIter iter;
+
+    // Open the file for reading
+    inFile=fopen(filename,"r");
+
+    if(inFile==NULL)
+    {
+        printf("An error occoured!");
+        exit(1);
+    }
+
+    while (fgets(buf, sizeof buf, inFile) != NULL)
+    {
+        mark = gtk_text_buffer_get_insert (buffer);
+        gtk_text_buffer_get_iter_at_mark (buffer, &iter, mark);
+        gtk_text_buffer_insert (buffer, &iter, buf, -1);
+    }
+
+    // Close the file
+    fclose(inFile);
 }
