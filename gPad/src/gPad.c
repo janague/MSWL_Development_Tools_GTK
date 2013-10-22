da60646283f9d39fd07879b09a3b1e169d4a889d
janague <janague@gmail.com>
2013-10-22 05:20:49 +0200

    Include menus and about function

--- gPad/src/gPad.c
+++ gPad/src/gPad.c
@@ -30,6 +30,12 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
+//_____________________________________________________________________________
+// Configuration
+//
+// Version
+#define GP_VERSION "0.2.0"
+
 // Set default values
 #define GP_DEFAULT_WIDTH 600
 #define GP_DEFAULT_HEIGHT 400
@@ -37,6 +43,9 @@
 // set the buffer size that it uses for reading the file.
 #define GP_BUFFER_SIZE 1024
 
+//_____________________________________________________________________________
+// Evente handler
+//
 // on_delete_event: Manage delete event
 static gboolean
 on_delete_event (   GtkWidget *widget,
@@ -47,6 +56,77 @@ on_delete_event (   GtkWidget *widget,
 
     return GDK_EVENT_PROPAGATE;
 }
+
+// activate_action: Catch action from menu entry
+// TODO: Complete each action
+static void
+activate_action (GtkAction *action)
+{
+      g_message ("Action \"%s\" activated", gtk_action_get_name (action));
+}
+
+static void about_cb (  GtkAction *action,
+                        GtkWidget *window);
+
+
+//_____________________________________________________________________________
+// Widget configuration
+//
+// Menus
+static GtkActionEntry entries[] = {
+  { "FileMenu", NULL, "_File" },               /* name, stock id, label */
+  { "HelpMenu", NULL, "_Help" },               /* name, stock id, label */
+  { "New", GTK_STOCK_NEW,                      /* name, stock id */
+    "_New", "<control>N",                      /* label, accelerator */
+    "Create a new file",                       /* tooltip */
+    G_CALLBACK (activate_action) },
+  { "Open", GTK_STOCK_OPEN,                    /* name, stock id */
+    "_Open","<control>O",                      /* label, accelerator */
+    "Open a file",                             /* tooltip */
+    G_CALLBACK (activate_action) },
+  { "Save", GTK_STOCK_SAVE,                    /* name, stock id */
+    "_Save","<control>S",                      /* label, accelerator */
+    "Save current file",                       /* tooltip */
+    G_CALLBACK (activate_action) },
+  { "SaveAs", GTK_STOCK_SAVE,                  /* name, stock id */
+    "Save _As...", NULL,                       /* label, accelerator */
+    "Save to a file",                          /* tooltip */
+    G_CALLBACK (activate_action) },
+  { "Quit", GTK_STOCK_QUIT,                    /* name, stock id */
+    "_Quit", "<control>Q",                     /* label, accelerator */
+    "Quit",                                    /* tooltip */
+    G_CALLBACK (activate_action) },
+  { "About", NULL,                             /* name, stock id */
+    "_About", "<control>A",                    /* label, accelerator */
+    "About",                                   /* tooltip */
+    G_CALLBACK (about_cb) },
+};
+static guint n_entries = G_N_ELEMENTS (entries);
+
+// User interface definition
+static const gchar *ui_info =
+"<ui>"
+"  <menubar name='MenuBar'>"
+"    <menu action='FileMenu'>"
+"      <menuitem action='New'/>"
+"      <menuitem action='Open'/>"
+"      <menuitem action='Save'/>"
+"      <menuitem action='SaveAs'/>"
+"      <separator/>"
+"      <menuitem action='Quit'/>"
+"    </menu>"
+"    <menu action='HelpMenu'>"
+"      <menuitem action='About'/>"
+"    </menu>"
+"  </menubar>"
+"  <toolbar  name='ToolBar'>"
+"    <toolitem action='Open'/>"
+"    <toolitem action='Quit'/>"
+"    <separator action='Sep1'/>"
+"    <toolitem action='Logo'/>"
+"  </toolbar>"
+"</ui>";
+
 
 // read_file: Read a file using standard io library
 void read_file ( char *filename,
@@ -61,6 +141,10 @@ main(int argc, char **argv)
     GtkWidget *window;
     GtkWidget *textView;
     GtkWidget *sWindow; // scrolled window
+    GtkWidget *box;
+    GtkUIManager *ui; // user interface
+    GtkActionGroup *actions; // actions
+    GError *error = NULL;
 
     GtkTextBuffer *buffer;
 
@@ -73,6 +157,36 @@ main(int argc, char **argv)
     gtk_window_set_default_size (GTK_WINDOW (window),
                                 GP_DEFAULT_WIDTH,
                                 GP_DEFAULT_HEIGHT);
+
+    // Set menu actions
+    actions = gtk_action_group_new ("Actions");
+    gtk_action_group_add_actions (actions, entries, n_entries, NULL);
+
+    // Create user interface manager
+    ui = gtk_ui_manager_new ();
+    gtk_ui_manager_insert_action_group (ui, actions, 0);
+    g_object_unref (actions);
+    gtk_window_add_accel_group (GTK_WINDOW (window),
+                                gtk_ui_manager_get_accel_group (ui));
+
+    if (!gtk_ui_manager_add_ui_from_string (ui, ui_info, -1, &error))
+    {
+        g_message ("building menus failed: %s", error->message);
+        g_error_free (error);
+    }
+
+    // Create a box
+    box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add (GTK_CONTAINER (window), box);
+
+    gtk_box_pack_start (GTK_BOX (box),
+                        gtk_ui_manager_get_widget (ui, "/MenuBar"),
+                        FALSE, FALSE, 0);
+
+    // Connect signal when destroy event is threw
+    g_signal_connect (  window,
+                        "destroy",
+                        G_CALLBACK (gtk_widget_destroyed), &window);
 
     // Connect signal when delete-event is threw
     g_signal_connect (  window,
@@ -91,7 +205,9 @@ main(int argc, char **argv)
                                     GTK_POLICY_AUTOMATIC);
 
     // Add scrolled window in application window
-    gtk_container_add(GTK_CONTAINER (window), sWindow);
+    // gtk_container_add(GTK_CONTAINER (window), sWindow);
+    // Add scrolled window in box
+    gtk_box_pack_start (GTK_BOX (box), sWindow, TRUE, TRUE, 0);
     // Add text view in scrolled window
     gtk_container_add(GTK_CONTAINER (sWindow), textView);
 
@@ -100,12 +216,18 @@ main(int argc, char **argv)
     read_file(argv[1], buffer);
 
     // Show widgets
+    // Show menus
+    // gtk_widget_show(ui);
     // Show text view
     gtk_widget_show (textView);
     // Show scrolled window
     gtk_widget_show (sWindow);
     // Show window
     gtk_widget_show (window);
+
+    gtk_widget_show (box);
+   // gtk_widget_show_all (window);
+   g_object_unref (ui);
 
     gtk_main();
 
@@ -144,4 +266,46 @@ void read_file ( char *filename,
 
     // Close the file
     fclose(inFile);
+}
+
+
+//_____________________________________________________________________________
+static void
+about_cb (GtkAction *action,
+          GtkWidget *window)
+{
+  GdkPixbuf *pixbuf;
+
+  const gchar *authors[] = {
+    "janague",
+    NULL
+  };
+
+  const gchar *documentors[] = {
+    "janague",
+    NULL
+  };
+
+  pixbuf = gdk_pixbuf_new_from_resource ("gPad.png", NULL);
+  /* We asser the existence of the pixbuf as we load it from a custom resource. */
+  g_assert (pixbuf);
+
+  gtk_show_about_dialog (GTK_WINDOW (window),
+                         "program-name", "gPad",
+                         "version", g_strdup_printf ("%s,\nRunning against GTK+ %d.%d.%d",
+                                                     GP_VERSION,
+                                                     gtk_get_major_version (),
+                                                     gtk_get_minor_version (),
+                                                     gtk_get_micro_version ()),
+                         "copyright", "(C) 2013 janague",
+                         "license-type",GTK_LICENSE_BSD,
+                         "website", "http://www.gPad.org",
+                         "comments", "Program to demonstrate GTK+ functions.",
+                         "authors", authors,
+                         "documenters", documentors,
+                         "logo", pixbuf,
+                         "title", "About GTK+ Code Demos",
+                         NULL);
+
+  g_object_unref (pixbuf);
 }
